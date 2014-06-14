4577861154d3423831d578114fe5682a85e5ee55
yKimisaki <y.kimisaki@live.jp>
2014-06-14 14:11:26 +0900

    RaycastでPlayerがあたったら処理できる

---
+++ Unity/UCS/Assets/Script/AI/Vision.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AI
+{
+    public class Vision : MonoBehaviour
+    {
+        // Update is called once per frame
+        void Update()
+        {
+            var ray = new Ray(this.transform.position, this.transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                var hitObject = hit.transform.gameObject;
+                if (hitObject == null)
+                    return;
+
+                var player = hitObject.GetComponent<Player>();
+                if (player != null)
+                {
+                    // プレイヤーを見つけた処理
+                }
+                else
+                {
+                    // 他を見つけた処理
+                }
+            }
+        }
+    }
+}
