9bfb1d10c2316a0d2bade68252a430c9242b5b22
yKimisaki <y.kimisaki@live.jp>
2014-06-15 11:33:48 +0900

    画像あれこれ

--- Unity/UCS/Assets/Scripts/Check_ScreenShot.cs
+++ Unity/UCS/Assets/Scripts/Check_ScreenShot.cs
@@ -1,6 +1,10 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Tao.OpenGl;
+using UnityEngine;
 
 public class Check_ScreenShot : MonoBehaviour {
 	private Texture2D tex;
@@ -11,8 +15,14 @@ public class Check_ScreenShot : MonoBehaviour {
 	void Start () {
 		tex2 = new List<Texture2D>();
 		//tex2[i] = Resources.Load("screenshot")  as Texture2D;
-		for(int j=0;;j++){
-			var texture = Resources.Load("screenshot" + j)  as Texture2D;
+        for (int j = 0; ; j++)
+        {
+            var path = "Assets/Resources/screenshot" + j + ".png";
+            if (!File.Exists(path))
+                break;
+
+            var ptr = GetPtr(path);
+            var texture = Texture2D.CreateExternalTexture(Screen.width, Screen.height, TextureFormat.ARGB32, false, false, ptr);//Resources.Load("screenshot" + j)  as Texture2D;
 			if(texture != null){
 				tex2.Add(texture);
 			}
@@ -35,4 +45,53 @@ public class Check_ScreenShot : MonoBehaviour {
 			Application.LoadLevel("collection");
 		}
 	}
+
+    IntPtr GetPtr(string fileName)
+    {
+        var bitmap = new Bitmap(fileName);
+        bitmap.RotateFlip(RotateFlipType.RotateNoneFlipY);
+        var data = bitmap.LockBits(
+            new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+        var bytes = new byte[bitmap.Width * bitmap.Height * 4];
+        System.Runtime.InteropServices.Marshal.Copy(data.Scan0, bytes, 0, bytes.Length);
+        bitmap.UnlockBits(data);
+
+        byte r, b;
+        int n;
+
+        for (int i = 0; i < bitmap.Width * bitmap.Height; ++i)
+        {
+            n = i * 4;
+            b = bytes[n];
+            r = bytes[n + 2];
+
+            bytes[n] = r;
+            bytes[n + 2] = b;
+        }
+
+        var name = new uint[1]; ;
+        Gl.glGenTextures(1, name);
+        Gl.glBindTexture(Gl.GL_TEXTURE_2D, name[0]);
+        Gl.glPixelStorei(Gl.GL_UNPACK_ALIGNMENT, 1);
+        Gl.glTexImage2D(
+                    Gl.GL_TEXTURE_2D,
+                    0,
+                    4,
+                    bitmap.Width, bitmap.Height,
+                    0,
+                    Gl.GL_RGBA,
+                    Gl.GL_UNSIGNED_BYTE,
+                    bytes
+                );
+
+        Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_LINEAR);
+        Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_LINEAR);
+
+        Gl.glBindTexture(Gl.GL_TEXTURE_2D, name[0]);
+
+        IntPtr ptr = IntPtr.Zero;
+        Gl.glGetTexImage(Gl.GL_TEXTURE_2D, 0, Gl.GL_DEPTH_COMPONENT, Gl.GL_UNSIGNED_BYTE, ptr);
+        
+        return data.Scan0;
+    }
 }
