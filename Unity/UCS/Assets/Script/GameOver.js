ce9ed8663de4500473fd83c0334ae90547446b16
yKimisaki <y.kimisaki@live.jp>
2014-06-15 12:33:54 +0900

    たいとるしーん

---
+++ Unity/UCS/Assets/Script/GameOver.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+// deathCountは本来の名前に書き直して下さい
+static var deathCount: int;
+
+// ①文字のパラメータ
+var style : GUIStyle ;
+
+function Start () {
+	deathCount = 0;
+	
+	style.fontSize=40;
+	//style.normal.textColor.red;
+}
+
+function Update () {
+	guiText.text = deathCount.ToString();
+}
+
+// ②条件を満たしたら「ＧＡＭＥ　ＯＶＥＲ」の文字を出す
+function OnGUI () {
+	if( deathCount >= 20 )
+	{
+	
+		GUI.Label( Rect ( Screen.width / 2 -100, 250, 200, 80), "GAME OVER" , style );
+		if( GUI.Button(Rect ( Screen.width / 2 -100, 200, 200, 30), "Go title" ))
+		{
+			Application.LoadLevel("title");
+		}
+	}
+}
---
+++ Unity/UCS/Assets/Script/Title.js
@@ -0,0 +1,18 @@
+﻿//#pragma SystemCollections
+#pragma strict
+
+//var sound:AudioClip;
+
+// ①GUIの描画
+function OnGUI () {
+	//audio.Play();
+  
+	// ②ボタンの作成
+	if( GUI.Button(Rect (Screen.width / 2 -100, 200, 200, 30), "GAME START"))
+	{
+		// ③ボタンが押された後シーンを切り替え
+		Application.LoadLevel( "main" );
+
+	}
+	
+}
