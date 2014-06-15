acfd3c7afac52db1929ad5cde807dcff0b0c5770
yKimisaki <y.kimisaki@live.jp>
2014-06-15 11:34:43 +0900

    ゲームシーン終了のお知らせ
    Conflicts:
    	Unity/UCS/Assets/Scene/testanimetion.unity

---
+++ Unity/UCS/Assets/Script/GameExit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AI
+{
+    public class GameExit : MonoBehaviour
+    {
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
